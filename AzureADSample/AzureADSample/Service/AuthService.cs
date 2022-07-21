ecf09bf3b822dfce6c5eefee3c0575616a0d913a
Giovanny Hernandez <87216285+officialgio@users.noreply.github.com>
2022-07-21 08:55:21 -0700

    Uploaded Project File

---
+++ AzureADSample/AzureADSample/Service/AuthService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureADSample.Service;
+public class AuthService
+{
+    public static IPublicClientApplication authenticationClient { get; private set;}
+    public AuthService()
+    {
+        authenticationClient = PublicClientApplicationBuilder.Create(Constants.ClientId)
+            //.WithB2CAuthority(Constants.AuthoritySignIn) // uncomment to support B2C
+            .WithRedirectUri($"msal{Constants.ClientId}://auth")
+            .Build();
+    }
+
+    // User must login successfully to get a token 
+    public async Task<AuthenticationResult> LoginAsync(CancellationToken cancellationToken)
+    {
+        AuthenticationResult result;
+        try
+        {
+            result = await authenticationClient
+                .AcquireTokenInteractive(Constants.Scopes)
+                .WithPrompt(Prompt.ForceLogin) //  command builder => prompt user for credemtials
+#if ANDROID
+                .WithParentActivityOrWindow(Microsoft.Maui.ApplicationModel.Platform.CurrentActivity)
+#endif
+                .ExecuteAsync(cancellationToken);
+            return result;
+        }
+        catch (MsalClientException)
+        {
+            return null;
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# AuthApplicationSample
+<h1 align="center">Simple Azure AD Authentication Sample</h1>
+
+<div align="center">
+Built in .NET MAUI and used Azure AD for Auth Service. This sample only demonstrates Authentication with using Azure AD (Microsoft Login Only) </br></br>
+</div>
+
+##  MSAL Login
+<p align="center">
+    <img width="400" src="./screenshots/Start.png">
+</p>
+
+##  Existing Account Login
+<p align="center">
+    <img width="400" src="./screenshots/ExistingAccount.gif">
+</p>
+
+
+## Nugget Packages
+- `Microsoft.Identity.Client`
+- `System.IdentityModel.Tokens.Jwt`
